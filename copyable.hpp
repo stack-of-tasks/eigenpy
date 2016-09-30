245a0a847fd29867c5fa4b56fa90454880304464
jcarpent <jcarpent@laas.fr>
2016-09-30 10:58:19 +0200

    [Python] Add copyable method to create a raw copy of Spatial classes
    [Python] Introduce struct to expose both std::vector and aligned_vector
    
    all: change __se3_* for __pinocchio_* for pragma once
    
    all: change namespace from se3 to pinocchio
    
    The previous se3 namespace is now deprecated but can still be used
    
    all: remove license from files
    
    [bindings][pickling] add support for vector<T> aligned_vector<T> map<T> pickling.
    update model and data classes to make their members picklable
    
    [bindings][pickling] 1) fix includes. make picklemap borrow from picklevector
    
    2) use pickle support for rest of data and some more of model vectors
    
    python: add converter from Python list to std::vector or aligned_vector
    
    python: fix issue on GCC related to namespace bp
    
    python: fix namespace issue
    
    python/vector: add conversion tolist for std::vector
    
    python/utils: return by reference when creating list
    
    python/utils: add return of the class for StdVector helpers
    
    python: fix return type
    
    python/pickle: fix pickling of std::map
    
    python: fix potential bug with C++11
    
    python: make the code compliant for casadi bindings
    
    python: fix arg name
    
    python/pickle: fix compatibility issue
    
    Compilation issues with recent version of Boost
    
    python: add copy constructor and method to std::vector
    
    python: expose operator{==,!=} for std::vector
    
    Revert "python: expose operator{==,!=} for std::vector"
    
    This reverts commit 6710dd4758791b3eee1ac444945e7f507aece7f6.
    
    python/utils: update std-vector exposition
    
    python: fix issues with const reference as input argument
    
    python: fix pickling of Pinocchio data structure

---
+++ copyable.hpp
@@ -0,0 +1,34 @@
+//
+// Copyright (c) 2016-2021 CNRS INRIA
+//
+
+#ifndef __pinocchio_python_utils_copyable_hpp__
+#define __pinocchio_python_utils_copyable_hpp__
+
+#include <boost/python.hpp>
+
+namespace pinocchio
+{
+  namespace python
+  {
+    
+    namespace bp = boost::python;
+    
+    ///
+    /// \brief Add the Python method copy to allow a copy of this by calling the copy constructor.
+    ///
+    template<class C>
+    struct CopyableVisitor
+    : public bp::def_visitor< CopyableVisitor<C> >
+    {
+      template<class PyClass>
+      void visit(PyClass & cl) const
+      { cl.def("copy",&copy,bp::arg("self"),"Returns a copy of *this."); }
+      
+    private:
+      static C copy(const C & self) { return C(self); }
+    };
+  } // namespace python
+} // namespace pinocchio
+
+#endif // ifndef __pinocchio_python_utils_copyable_hpp__
---
+++ pickle-vector.hpp
@@ -0,0 +1,51 @@
+//
+// Copyright (c) 2019-2020 CNRS INRIA
+//
+
+#ifndef __pinocchio_python_utils_pickle_vector_hpp__
+#define __pinocchio_python_utils_pickle_vector_hpp__
+
+#include <boost/python.hpp>
+#include <boost/python/tuple.hpp>
+#include <boost/python/stl_iterator.hpp>
+
+namespace pinocchio
+{
+  namespace python
+  {
+    ///
+    /// \brief Create a pickle interface for the std::vector and aligned vector
+    ///
+    /// \tparam VecType Vector Type to pickle
+    ///
+    template<typename VecType>
+    struct PickleVector : boost::python::pickle_suite
+    {
+      static boost::python::tuple getinitargs(const VecType&)
+      { return boost::python::make_tuple(); }
+      
+      static boost::python::tuple getstate(boost::python::object op)
+      {
+        return boost::python::make_tuple(boost::python::list(boost::python::extract<const VecType&>(op)()));
+      }
+      
+      static void setstate(boost::python::object op, boost::python::tuple tup)
+      {
+        if(boost::python::len(tup) > 0)
+        {
+          VecType & o = boost::python::extract<VecType&>(op)();
+          boost::python::stl_input_iterator<typename VecType::value_type> begin(tup[0]), end;
+          while(begin != end)
+          {
+             o.push_back(*begin);
+             ++begin;
+          }
+        }
+      }
+      
+      static bool getstate_manages_dict() { return true; }
+    };
+  }
+}
+
+#endif // ifndef __pinocchio_python_utils_pickle_vector_hpp__
