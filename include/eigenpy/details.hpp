fa17abcbb566a150ad006d90b01423acc5feabf1
Justin Carpentier <justin.carpentier@inria.fr>
2019-12-10 14:19:38 +0100

    core: fix issue when converting form Matrix to Eigen::MatrixBase<Matrix>

--- include/eigenpy/details.hpp
+++ include/eigenpy/details.hpp
@@ -49,6 +49,37 @@ struct implicit<Eigen::MatrixBase<MatType>,MatType>
   }
 };
 
+template<class MatType>
+struct implicit<MatType,Eigen::MatrixBase<MatType> >
+{
+  typedef MatType Source;
+  typedef Eigen::MatrixBase<MatType> Target;
+  
+  static void* convertible(PyObject* obj)
+  {
+    // Find a converter which can produce a Source instance from
+    // obj. The user has told us that Source can be converted to
+    // Target, and instantiating construct() below, ensures that
+    // at compile-time.
+    return implicit_rvalue_convertible_from_python(obj, registered<Source>::converters)
+    ? obj : 0;
+  }
+  
+  static void construct(PyObject* obj, rvalue_from_python_stage1_data* data)
+  {
+    void* storage = ((rvalue_from_python_storage<Source>*)data)->storage.bytes;
+    
+    arg_from_python<Source> get_source(obj);
+    bool convertible = get_source.convertible();
+    BOOST_VERIFY(convertible);
+    
+    new (storage) Source(get_source());
+    
+    // record successful construction
+    data->convertible = storage;
+  }
+};
+
 }}} // namespace boost::python::converter
 
 #define GET_PY_ARRAY_TYPE(array) PyArray_ObjectType(reinterpret_cast<PyObject *>(array), 0)
