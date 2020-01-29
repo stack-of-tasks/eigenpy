4e7cbeb3397720161c20f103dea99cbe4b25bfc1
Justin Carpentier <justin.carpentier@inria.fr>
2020-01-29 17:52:34 +0100

    test: add test of EigenSolver

---
+++ unittest/python/test_eigen_solver.py
@@ -0,0 +1,16 @@
+import eigenpy
+eigenpy.switchToNumpyArray()
+
+import numpy as np
+import numpy.linalg as la
+
+dim = 100
+A = np.random.rand(dim,dim)
+
+es = eigenpy.EigenSolver(A)
+
+V = es.eigenvectors()
+D = es.eigenvalues()
+
+assert eigenpy.is_approx(A.dot(V).real,V.dot(np.diag(D)).real)
+assert eigenpy.is_approx(A.dot(V).imag,V.dot(np.diag(D)).imag)
