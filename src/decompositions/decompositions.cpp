9e090959b9baa6b7fd5a2bd96fa48de8bb5e9550
Justin Carpentier <justin.carpentier@inria.fr>
2020-01-29 18:56:21 +0100

    decompositions: expose DecompositionOptions

--- src/decompositions/decompositions.cpp
+++ src/decompositions/decompositions.cpp
@@ -15,6 +15,21 @@ namespace eigenpy
     namespace bp = boost::python;
     
     EigenSolverVisitor<Eigen::MatrixXd>::expose("EigenSolver");
+
+    {
+      using namespace Eigen;
+      bp::enum_<DecompositionOptions>("DecompositionOptions")
+      .value("ComputeFullU",ComputeFullU)
+      .value("ComputeThinU",ComputeThinU)
+      .value("ComputeFullV",ComputeFullV)
+      .value("ComputeThinV",ComputeThinV)
+      .value("EigenvaluesOnly",EigenvaluesOnly)
+      .value("ComputeEigenvectors",ComputeEigenvectors)
+      .value("Ax_lBx",Ax_lBx)
+      .value("ABx_lx",ABx_lx)
+      .value("BAx_lx",BAx_lx)
+      ;
+    }
     
   }
 } // namespace eigenpy
