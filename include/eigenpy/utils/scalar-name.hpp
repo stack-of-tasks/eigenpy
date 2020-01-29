0f6d88d345d8a5d93f8cd64043f8a128dda7f2a2
Justin Carpentier <justin.carpentier@inria.fr>
2020-01-29 16:11:06 +0100

    utils: add name methods for Scalar types

---
+++ include/eigenpy/utils/scalar-name.hpp
@@ -0,0 +1,44 @@
+/*
+* Copyright 2020 INRIA
+*/
+
+#ifndef __eigenpy_utils_scalar_name_hpp__
+#define __eigenpy_utils_scalar_name_hpp__
+
+#include <string>
+#include <complex>
+
+namespace eigenpy
+{
+  template<typename Scalar>
+  struct scalar_name
+  {
+    static std::string shortname();
+  };
+
+  template<>
+  struct scalar_name<float>
+  {
+    static std::string shortname() { return "f"; };
+  };
+
+  template<>
+  struct scalar_name<double>
+  {
+    static std::string shortname() { return "d"; };
+  };
+
+  template<>
+  struct scalar_name<long double>
+  {
+    static std::string shortname() { return "ld"; };
+  };
+
+  template<typename Scalar>
+  struct scalar_name< std::complex<Scalar> >
+  {
+    static std::string shortname() { return "c" + scalar_name<Scalar>(); };
+  };
+}
+
+#endif // ifndef __eigenpy_utils_scalar_name_hpp__
