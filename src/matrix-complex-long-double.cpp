9aa210faaaa123eab10692e5fd40415d1169b20e
Justin Carpentier <justin.carpentier@inria.fr>
2020-02-05 10:49:55 +0100

    core: reduce the memory overload when compiling

---
+++ src/matrix-complex-long-double.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2020 INRIA
+ */
+
+#include "eigenpy/eigenpy.hpp"
+
+namespace eigenpy
+{
+  void exposeMatrixComplexLongDouble()
+  {
+    exposeType<std::complex<long double> >();
+  }
+}
