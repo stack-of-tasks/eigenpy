a29f6f85b695d98b00f50dc82940562b9f938050
Justin Carpentier <justin.carpentier@inria.fr>
2020-02-20 12:01:57 +0100

    python: use original signature of is_approx

--- include/eigenpy/utils/is-approx.hpp
+++ include/eigenpy/utils/is-approx.hpp
@@ -1,6 +1,6 @@
 /*
-* Copyright 2020 INRIA
-*/
+ * Copyright 2020 INRIA
+ */
 
 #ifndef __eigenpy_utils_scalar_is_approx_hpp__
 #define __eigenpy_utils_scalar_is_approx_hpp__
@@ -10,15 +10,16 @@
 namespace eigenpy
 {
   template<typename MatrixType1, typename MatrixType2>
-  inline EIGEN_DONT_INLINE bool is_approx(const MatrixType1 & mat1,
-                                          const MatrixType2 & mat2,
+  inline EIGEN_DONT_INLINE bool is_approx(const Eigen::MatrixBase<MatrixType1> & mat1,
+                                          const Eigen::MatrixBase<MatrixType2> & mat2,
                                           const typename MatrixType1::Scalar & prec)
   {
-    return mat1.derived().isApprox(mat2.derived(),prec);
+    return mat1.isApprox(mat2,prec);
   }
 
   template<typename MatrixType1, typename MatrixType2>
-  inline bool is_approx(const MatrixType1 & mat1, const MatrixType2 & mat2)
+  inline EIGEN_DONT_INLINE bool is_approx(const Eigen::MatrixBase<MatrixType1> & mat1,
+                                          const Eigen::MatrixBase<MatrixType2> & mat2)
   {
     return is_approx(mat1,mat2,Eigen::NumTraits<typename MatrixType1::Scalar>::dummy_precision());
   }
