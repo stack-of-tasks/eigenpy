db84212f60debea0010c3007cc9cbde0cdccda38
Justin Carpentier <justin.carpentier@inria.fr>
2020-02-24 00:33:43 +0100

    core: fix EIGENPY_DEFAULT_ALIGNMENT_VALUE

--- include/eigenpy/fwd.hpp
+++ include/eigenpy/fwd.hpp
@@ -14,14 +14,10 @@
   #include "eigenpy/numpy.hpp"
 #undef NO_IMPORT_ARRAY
 
-#ifdef NPY_ALIGNED
 #if EIGEN_VERSION_AT_LEAST(3,2,90)
   #define EIGENPY_DEFAULT_ALIGNMENT_VALUE Eigen::Aligned16
 #else
   #define EIGENPY_DEFAULT_ALIGNMENT_VALUE Eigen::Aligned
-#endif
-#else
-  #define EIGENPY_DEFAULT_ALIGNMENT_VALUE Eigen::Unaligned
 #endif
 
 #include "eigenpy/expose.hpp"
