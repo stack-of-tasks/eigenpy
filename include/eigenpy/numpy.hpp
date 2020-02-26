7a57c1983021e4718d769ff2c6d093fbdc7a045d
Justin Carpentier <justin.carpentier@inria.fr>
2020-02-26 09:08:45 +0100

    core: patch Win32
    which is not able to retrieve EIGENPY_ARRAY_API when compiling other module because the symbol is not exported

--- include/eigenpy/numpy.hpp
+++ include/eigenpy/numpy.hpp
@@ -19,11 +19,32 @@
 
 #include <numpy/noprefix.h>
 
-#define EIGENPY_GET_PY_ARRAY_TYPE(array) PyArray_ObjectType(reinterpret_cast<PyObject *>(array), 0)
+#if defined _WIN32 || defined __CYGWIN__
+  #define EIGENPY_GET_PY_ARRAY_TYPE(array) \
+    call_PyArray_ObjectType(reinterpret_cast<PyObject *>(array), 0)
+#else
+  #define EIGENPY_GET_PY_ARRAY_TYPE(array) \
+    PyArray_ObjectType(reinterpret_cast<PyObject *>(array), 0)
+#endif
 
 namespace eigenpy
 {
   void EIGENPY_DLLEXPORT import_numpy();
 }
+
+#if defined _WIN32 || defined __CYGWIN__
+namespace eigenpy
+{
+  EIGENPY_DLLEXPORT PyArrayObject*  call_PyArray_SimpleNew(npy_intp nd, npy_intp * shape, NPY_TYPES np_type);
+
+  EIGENPY_DLLEXPORT PyArrayObject* call_PyArray_New(npy_intp nd, npy_intp * shape, NPY_TYPES np_type, void * data_ptr, npy_intp options);
+
+  EIGENPY_DLLEXPORT int call_PyArray_ObjectType(PyObject *, int);
+}
+#else
+  #define call_PyArray_SimpleNew PyArray_SimpleNew
+  #define call_PyArray_New(nd,shape,np_type,data_ptr,options) \
+    PyArray_New(&PyArray_Type,nd,shape,np_type,NULL,data_ptr,0,options,NULL)
+#endif
 
 #endif // ifndef __eigenpy_numpy_hpp__
--- src/numpy.cpp
+++ src/numpy.cpp
@@ -14,4 +14,23 @@ namespace eigenpy
       PyErr_SetString(PyExc_ImportError, "numpy.core.multiarray failed to import");
     }
   }
+
+#if defined _WIN32 || defined __CYGWIN__
+
+  PyArrayObject* call_PyArray_SimpleNew(npy_intp nd, npy_intp * shape, NPY_TYPES np_type)
+  {
+    return PyArray_SimpleNew(nd,shape,np_type);
+  }
+
+  PyArrayObject* call_PyArray_New(npy_intp nd, npy_intp * shape, NPY_TYPES np_type, void * data_ptr, npy_intp options)
+  {
+    return PyArray_New(&PyArray_Type,nd,shape,np_type,NULL,data_ptr,0,options,NULL);
+  }
+  
+  int call_PyArray_ObjectType(PyObject * obj, int val)
+  {
+    return PyArray_ObjectType(obj,val);
+  }
+
+#endif
 }
