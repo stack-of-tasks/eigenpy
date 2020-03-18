155d92ecfc77965e5e7371b174f81dccc36d1ff3
Justin Carpentier <justin.carpentier@inria.fr>
2020-03-18 18:11:48 +0100

    core: fix doc message

--- src/eigenpy.cpp
+++ src/eigenpy.cpp
@@ -48,13 +48,13 @@ namespace eigenpy
     bp::def("sharedMemory",
             (void (*)(const bool))NumpyType::sharedMemory,
             bp::arg("value"),
-            "Share the memory when converting Eigen::Matrix to numpy.array.");
+            "Share the memory when converting from Eigen to Numpy.");
     
     bp::def("sharedMemory",
             (bool (*)())NumpyType::sharedMemory,
-            "Status of the shared memory when converting Eigen::Matrix to numpy.array.\n"
+            "Status of the shared memory when converting from Eigen to Numpy.\n"
             "If True, the memory is shared when converting an Eigen::Matrix to a numpy.array.\n"
-            "Otherwise, a deep copy of the Eigen::Matrix is performed");
+            "Otherwise, a deep copy of the Eigen::Matrix is performed.");
     
     bp::def("seed",&seed,bp::arg("seed_value"),
             "Initialize the pseudo-random number generator with the argument seed_value.");
