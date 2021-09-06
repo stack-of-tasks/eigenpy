2f67236be11a9991cad2d42a55a0c2e5a90e4488
Justin Carpentier <justin.carpentier@inria.fr>
2021-09-06 21:19:07 +0200

    test: fix user_type for Windows

--- unittest/user_type.cpp
+++ unittest/user_type.cpp
@@ -198,12 +198,14 @@ BOOST_PYTHON_MODULE(user_type)
   eigenpy::registerCast<double,DoubleType>(true);
   eigenpy::registerCast<DoubleType,int32_t>(false);
   eigenpy::registerCast<int32_t,DoubleType>(true);
-  eigenpy::registerCast<DoubleType,int64_t>(false);
-  eigenpy::registerCast<int64_t,DoubleType>(true);
+  eigenpy::registerCast<DoubleType,long long>(false);
+  eigenpy::registerCast<long long,DoubleType>(true);
   eigenpy::registerCast<FloatType,double>(true);
   eigenpy::registerCast<double,FloatType>(false);
-  eigenpy::registerCast<FloatType,int64_t>(false);
-  eigenpy::registerCast<int64_t,FloatType>(true);
+  eigenpy::registerCast<FloatType,long long>(false);
+  eigenpy::registerCast<long long,FloatType>(true);
+  eigenpy::registerCast<FloatType,int32_t>(false);
+  eigenpy::registerCast<int32_t,FloatType>(true);
 
   bp::implicitly_convertible<double,DoubleType>();
   bp::implicitly_convertible<DoubleType,double>();
