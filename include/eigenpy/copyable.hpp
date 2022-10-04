953660b3464542bf835f455759a453df5e2c1d01
ManifoldFR <wilson.jallet@polytechnique.org>
2022-10-04 17:04:16 +0200

    Run pre-commit on copyable, pickle-vector, std-aligned-vector
    remove references to aligned

--- include/eigenpy/copyable.hpp
+++ include/eigenpy/copyable.hpp
@@ -7,28 +7,26 @@
 
 #include <boost/python.hpp>
 
-namespace pinocchio
-{
-  namespace python
-  {
-    
-    namespace bp = boost::python;
-    
-    ///
-    /// \brief Add the Python method copy to allow a copy of this by calling the copy constructor.
-    ///
-    template<class C>
-    struct CopyableVisitor
-    : public bp::def_visitor< CopyableVisitor<C> >
-    {
-      template<class PyClass>
-      void visit(PyClass & cl) const
-      { cl.def("copy",&copy,bp::arg("self"),"Returns a copy of *this."); }
-      
-    private:
-      static C copy(const C & self) { return C(self); }
-    };
-  } // namespace python
-} // namespace pinocchio
+namespace pinocchio {
+namespace python {
 
-#endif // ifndef __pinocchio_python_utils_copyable_hpp__
+namespace bp = boost::python;
+
+///
+/// \brief Add the Python method copy to allow a copy of this by calling the
+/// copy constructor.
+///
+template <class C>
+struct CopyableVisitor : public bp::def_visitor<CopyableVisitor<C> > {
+  template <class PyClass>
+  void visit(PyClass& cl) const {
+    cl.def("copy", &copy, bp::arg("self"), "Returns a copy of *this.");
+  }
+
+ private:
+  static C copy(const C& self) { return C(self); }
+};
+}  // namespace python
+}  // namespace pinocchio
+
+#endif  // ifndef __pinocchio_python_utils_copyable_hpp__
--- include/eigenpy/std-aligned-vector.hpp
+++ include/eigenpy/std-aligned-vector.hpp
@@ -8,77 +8,78 @@
 #include <boost/python.hpp>
 #include <string>
 
-#include "pinocchio/container/aligned-vector.hpp"
-
 #include "pinocchio/bindings/python/utils/pickle-vector.hpp"
 #include "pinocchio/bindings/python/utils/std-vector.hpp"
+#include "pinocchio/container/aligned-vector.hpp"
 
-namespace pinocchio
-{
-  namespace python
-  {
-    
-    ///
-    /// \brief Expose an container::aligned_vector from a type given as template argument.
-    ///
-    /// \tparam T Type to expose as container::aligned_vector<T>.
-    /// \tparam EnableFromPythonListConverter Enables the conversion from a Python list to a container::aligned_vector<T>.
-    ///
-    /// \sa StdAlignedVectorPythonVisitor
-    ///
-    template<class T, bool NoProxy = false, bool EnableFromPythonListConverter = true>
-    struct StdAlignedVectorPythonVisitor
-    : public ::boost::python::vector_indexing_suite<typename container::aligned_vector<T>,NoProxy, internal::contains_vector_derived_policies<typename container::aligned_vector<T>,NoProxy> >
-    , public StdContainerFromPythonList< container::aligned_vector<T> >
-    {
-      typedef container::aligned_vector<T> vector_type;
-      typedef StdContainerFromPythonList<vector_type,NoProxy> FromPythonListConverter;
-      typedef T value_type;
-      
-      static void expose(const std::string & class_name,
-                         const std::string & doc_string = "")
-      {
-        expose(class_name,doc_string,EmptyPythonVisitor());
-      }
-      
-      template<typename VisitorDerived>
-      static void expose(const std::string & class_name,
-                         const boost::python::def_visitor<VisitorDerived> & visitor)
-      {
-        expose(class_name,"",visitor);
-      }
-      
-      template<typename VisitorDerived>
-      static void expose(const std::string & class_name,
-                         const std::string & doc_string,
-                         const boost::python::def_visitor<VisitorDerived> & visitor)
-      {
-        namespace bp = boost::python;
-        if(!register_symbolic_link_to_registered_type<vector_type>())
-        {
-          bp::class_<vector_type> cl(class_name.c_str(),doc_string.c_str());
-          cl
-          .def(StdAlignedVectorPythonVisitor())
-          
-          .def(bp::init<size_t, const value_type &>(bp::args("self","size","value"),"Constructor from a given size and a given value."))
-          .def(bp::init<const vector_type &>(bp::args("self","other"),"Copy constructor"))
-          
-          .def("tolist",&FromPythonListConverter::tolist,bp::arg("self"),
+namespace pinocchio {
+namespace python {
+
+///
+/// \brief Expose an container::aligned_vector from a type given as template
+/// argument.
+///
+/// \tparam T Type to expose as container::aligned_vector<T>.
+/// \tparam EnableFromPythonListConverter Enables the conversion from a Python
+/// list to a container::aligned_vector<T>.
+///
+/// \sa StdAlignedVectorPythonVisitor
+///
+template <class T, bool NoProxy = false,
+          bool EnableFromPythonListConverter = true>
+struct StdAlignedVectorPythonVisitor
+    : public ::boost::python::vector_indexing_suite<
+          typename container::aligned_vector<T>, NoProxy,
+          internal::contains_vector_derived_policies<
+              typename container::aligned_vector<T>, NoProxy> >,
+      public StdContainerFromPythonList<container::aligned_vector<T> > {
+  typedef container::aligned_vector<T> vector_type;
+  typedef StdContainerFromPythonList<vector_type, NoProxy>
+      FromPythonListConverter;
+  typedef T value_type;
+
+  static void expose(const std::string &class_name,
+                     const std::string &doc_string = "") {
+    expose(class_name, doc_string, EmptyPythonVisitor());
+  }
+
+  template <typename VisitorDerived>
+  static void expose(
+      const std::string &class_name,
+      const boost::python::def_visitor<VisitorDerived> &visitor) {
+    expose(class_name, "", visitor);
+  }
+
+  template <typename VisitorDerived>
+  static void expose(
+      const std::string &class_name, const std::string &doc_string,
+      const boost::python::def_visitor<VisitorDerived> &visitor) {
+    namespace bp = boost::python;
+    if (!register_symbolic_link_to_registered_type<vector_type>()) {
+      bp::class_<vector_type> cl(class_name.c_str(), doc_string.c_str());
+      cl.def(StdAlignedVectorPythonVisitor())
+
+          .def(bp::init<size_t, const value_type &>(
+              bp::args("self", "size", "value"),
+              "Constructor from a given size and a given value."))
+          .def(bp::init<const vector_type &>(bp::args("self", "other"),
+                                             "Copy constructor"))
+
+          .def("tolist", &FromPythonListConverter::tolist, bp::arg("self"),
                "Returns the aligned_vector as a Python list.")
           .def(visitor)
 #ifndef PINOCCHIO_PYTHON_NO_SERIALIZATION
           .def_pickle(PickleVector<vector_type>())
 #endif
-          .def(CopyableVisitor<vector_type>())
-          ;
-          
-          // Register conversion
-          if(EnableFromPythonListConverter)
-            FromPythonListConverter::register_converter();
-        }
-      }
-    };
-  } // namespace python
-} // namespace pinocchio
+          .def(CopyableVisitor<vector_type>());
 
-#endif // ifndef __pinocchio_python_utils_std_aligned_vector_hpp__
+      // Register conversion
+      if (EnableFromPythonListConverter)
+        FromPythonListConverter::register_converter();
+    }
+  }
+};
+}  // namespace python
+}  // namespace pinocchio
+
+#endif  // ifndef __pinocchio_python_utils_std_aligned_vector_hpp__
