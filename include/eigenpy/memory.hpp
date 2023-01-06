8cd3f654a8b3f3e76c0baeb97090d8659a6c9aad
Justin Carpentier <justin.carpentier@inria.fr>
2023-01-06 09:34:47 +0100

    core: make generic alignment of struct

--- include/eigenpy/memory.hpp
+++ include/eigenpy/memory.hpp
@@ -15,6 +15,12 @@ static inline void _Py_SET_SIZE(PyVarObject* ob, Py_ssize_t size) {
 #define Py_SET_SIZE(ob, size) _Py_SET_SIZE((PyVarObject*)(ob), size)
 #endif
 
+#if EIGEN_DEFAULT_ALIGN_BYTES > 16
+#define EIGENPY_DEFAULT_ALIGN_BYTES EIGEN_DEFAULT_ALIGN_BYTES
+#else
+#define EIGENPY_DEFAULT_ALIGN_BYTES 16
+#endif
+
 /**
  * This section contains a convenience MACRO which allows an easy specialization
  * of Boost Python Object allocator for struct data types containing Eigen
@@ -42,7 +48,7 @@ static inline void _Py_SET_SIZE(PyVarObject* ob, Py_ssize_t size) {
                                                                                \
     union {                                                                    \
       align_t align;                                                           \
-      char bytes[sizeof(Data) + 16];                                           \
+      char bytes[sizeof(Data) + EIGENPY_DEFAULT_ALIGN_BYTES];                  \
     } storage;                                                                 \
   };                                                                           \
                                                                                \
@@ -96,7 +102,9 @@ static inline void _Py_SET_SIZE(PyVarObject* ob, Py_ssize_t size) {
         void* storage, PyObject* instance,                                     \
         reference_wrapper<__VA_ARGS__ const> x) {                              \
       void* aligned_storage = reinterpret_cast<void*>(                         \
-          (reinterpret_cast<size_t>(storage) & ~(size_t(15))) + 16);           \
+          (reinterpret_cast<size_t>(storage) &                                 \
+           ~(size_t(EIGENPY_DEFAULT_ALIGN_BYTES - 1))) +                       \
+          EIGENPY_DEFAULT_ALIGN_BYTES);                                        \
       value_holder<__VA_ARGS__>* new_holder =                                  \
           new (aligned_storage) value_holder<__VA_ARGS__>(instance, x);        \
       return new_holder;                                                       \
