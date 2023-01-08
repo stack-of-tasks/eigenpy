0104a9ff761104f6011bc31e7ba8551702eb4f81
Justin Carpentier <justin.carpentier@inria.fr>
2023-01-08 12:04:19 +0100

    memory: set deprecated

--- include/eigenpy/memory.hpp
+++ include/eigenpy/memory.hpp
@@ -3,25 +3,21 @@
  * Copyright 2018-2023, INRIA
  */
 
+#include "eigenpy/fwd.hpp"
+
+EIGENPY_DEPRECATED_FILE(
+    "This header file is now useless and should not be included anymore.")
+
 #ifndef __eigenpy_memory_hpp__
 #define __eigenpy_memory_hpp__
-
-#include "eigenpy/fwd.hpp"
 
 /**
  * This section contains a convenience MACRO which allows an easy specialization
  * of Boost Python Object allocator for struct data types containing Eigen
  * objects and requiring strict alignment.
  */
-#define EIGENPY_DEFINE_STRUCT_ALLOCATOR_SPECIALIZATION(...)         \
-  namespace boost {                                                 \
-  namespace python {                                                \
-  namespace objects {                                               \
-  template <>                                                       \
-  struct instance<value_holder<__VA_ARGS__> >                       \
-      : ::eigenpy::aligned_instance<value_holder<__VA_ARGS__> > {}; \
-  }                                                                 \
-  }                                                                 \
-  }
+#define EIGENPY_DEFINE_STRUCT_ALLOCATOR_SPECIALIZATION(...)                  \
+  EIGENPY_DEPRECATED_MACRO(EIGENPY_DEFINE_STRUCT_ALLOCATOR_SPECIALIZATION(), \
+                           "it is no more needed.")
 
 #endif  // __eigenpy_memory_hpp__
