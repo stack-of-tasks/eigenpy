730f34f761263c648f5dabef3d66d346955b7d12
Justin Carpentier <justin.carpentier@inria.fr>
2023-01-09 14:08:59 +0100

    all: remove deprecarte EIGENPY_DEFINE_STRUCT_ALLOCATOR_SPECIALIZATION

--- src/angle-axis.cpp
+++ src/angle-axis.cpp
@@ -1,14 +1,10 @@
 /*
  * Copyright 2014-2019, CNRS
- * Copyright 2018-2019, INRIA
+ * Copyright 2018-2023, INRIA
  */
 
 #include "eigenpy/angle-axis.hpp"
-
 #include "eigenpy/geometry.hpp"
-#include "eigenpy/memory.hpp"
-
-EIGENPY_DEFINE_STRUCT_ALLOCATOR_SPECIALIZATION(Eigen::AngleAxisd)
 
 namespace eigenpy {
 void exposeAngleAxis() { expose<Eigen::AngleAxisd>(); }
--- src/quaternion.cpp
+++ src/quaternion.cpp
@@ -1,15 +1,11 @@
 /*
  * Copyright 2014-2019, CNRS
- * Copyright 2018-2022, INRIA
+ * Copyright 2018-2023, INRIA
  */
 
 #include <Eigen/Geometry>
 
 #include "eigenpy/geometry.hpp"
-#include "eigenpy/memory.hpp"
-
-EIGENPY_DEFINE_STRUCT_ALLOCATOR_SPECIALIZATION(Eigen::Quaterniond)
-
 #include "eigenpy/quaternion.hpp"
 
 namespace eigenpy {
