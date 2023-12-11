3847cc48212c4f14719949985f9502bf9057fe19
Justin Carpentier <justin.carpentier@inria.fr>
2023-12-11 18:14:17 +0100

    test: add test for std::pair

---
+++ unittest/python/test_std_pair.py
@@ -0,0 +1,4 @@
+from std_pair import std_pair_to_tuple
+
+t = (1, 2.0)
+assert std_pair_to_tuple(t) == t
---
+++ unittest/std_pair.cpp
@@ -0,0 +1,24 @@
+/// @file
+/// @copyright Copyright 2023 CNRS INRIA
+
+#include <eigenpy/eigenpy.hpp>
+#include <eigenpy/std-pair.hpp>
+#include <iostream>
+
+namespace bp = boost::python;
+
+template <typename T1, typename T2>
+bp::tuple std_pair_to_tuple(const std::pair<T1, T2>& pair) {
+  return bp::make_tuple(pair.first, pair.second);
+}
+
+BOOST_PYTHON_MODULE(std_pair) {
+  using namespace eigenpy;
+
+  enableEigenPy();
+
+  typedef std::pair<int, double> PairType;
+  StdPairConverter<PairType>::registration();
+
+  bp::def("std_pair_to_tuple", std_pair_to_tuple<int, double>);
+}
