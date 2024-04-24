da34179d16775a7975fd261e17425f4fac9715d9
Joris Vaillant <joris.vaillant@inria.fr>
2024-04-24 16:54:00 +0200

    core: Remove a CL dedicated hack to allow EigenToPy and EigenFromPy specialization on Windows
    core: Use same namespace scope

--- include/eigenpy/eigen-from-python.hpp
+++ include/eigenpy/eigen-from-python.hpp
@@ -290,16 +290,10 @@ struct eigen_from_py_impl<MatType, Eigen::MatrixBase<MatType> > {
   static void registration();
 };
 
-#ifdef EIGENPY_MSVC_COMPILER
-template <typename EigenType>
-struct EigenFromPy<EigenType,
-                   typename boost::remove_reference<EigenType>::type::Scalar>
-#else
-template <typename EigenType, typename _Scalar>
-struct EigenFromPy
-#endif
-    : eigen_from_py_impl<EigenType> {
-};
+template <typename EigenType,
+          typename Scalar =
+              typename boost::remove_reference<EigenType>::type::Scalar>
+struct EigenFromPy : eigen_from_py_impl<EigenType> {};
 
 template <typename MatType>
 void *eigen_from_py_impl<MatType, Eigen::MatrixBase<MatType> >::convertible(
