b1ca46f8b2b1a465fca95fb0dc873a9517409b4c
Justin Carpentier <justin.carpentier@inria.fr>
2024-09-03 14:28:39 +0200

    test: fix

--- unittest/python/test_type_info.py
+++ unittest/python/test_type_info.py
@@ -4,7 +4,4 @@ d = type_info.Dummy()
 assert d.type_info().pretty_name() == "Dummy"
 
 assert type_info.type_info(1).pretty_name() == "int"
-assert (
-    type_info.type_info("toto").pretty_name()
-    == "std::__1::basic_string<char, std::__1::char_traits<char>, std::__1::allocator<char>>"
-)
+assert "basic_string" in type_info.type_info("toto").pretty_name()
