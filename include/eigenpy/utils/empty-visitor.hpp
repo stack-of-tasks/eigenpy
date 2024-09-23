6020ba44bb31f88c7f6483e03799b78f7861636f
ManifoldFR <wilson.jallet@polytechnique.org>
2024-09-23 11:53:39 +0200

    Move EmptyPythonVisitor to its own header file

---
+++ include/eigenpy/utils/empty-visitor.hpp
@@ -0,0 +1,11 @@
+#include "eigenpy/fwd.hpp"
+
+namespace eigenpy {
+
+struct EmptyPythonVisitor
+    : public ::boost::python::def_visitor<EmptyPythonVisitor> {
+  template <class classT>
+  void visit(classT &) const {}
+};
+
+}  // namespace eigenpy
