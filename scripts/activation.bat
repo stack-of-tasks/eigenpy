c1667567458cf4a91f63eb5c3777799f4b0c9838
Joris Vaillant <joris.vaillant@inria.fr>
2024-10-04 12:00:13 +0200

    pixi: Use environment variable to improve genericity

--- scripts/activation.bat
+++ scripts/activation.bat
@@ -1,11 +1,8 @@
-REM When running CMake for the first time set build type to release
-set CMAKE_BUILD_TYPE="Release"
-
-REM Setup ccache
+:: Setup ccache
 set CMAKE_CXX_COMPILER_LAUNCHER=ccache
 
-REM Create compile_commands.json for language server
+:: Create compile_commands.json for language server
 set CMAKE_EXPORT_COMPILE_COMMANDS=1
 
-REM Activate color output with Ninja
+:: Activate color output with Ninja
 set CMAKE_COLOR_DIAGNOSTICS=1
--- scripts/activation.sh
+++ scripts/activation.sh
@@ -7,9 +7,6 @@ unset DEBUG_CPPFLAGS
 unset DEBUG_CXXFLAGS
 unset LDFLAGS
 
-# When running CMake for the first time set build type to release
-export CMAKE_BUILD_TYPE="Release"
-
 # Setup ccache
 export CMAKE_CXX_COMPILER_LAUNCHER=ccache
 
