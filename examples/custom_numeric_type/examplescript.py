c6048d3ec4c79ffb1a0038347f49bb6be64e11ed
silviana amethyst <1388063+ofloveandhate@users.noreply.github.com>
2024-11-25 15:29:28 +0100

    more things exercised in example script
    including
    * making a numpy array of custom type from an existing array of built-in type
    * calling c++ function that takes eigen matrix which modifies its argument
    * making an array of zeros without conversion from existing numpy array

--- examples/custom_numeric_type/examplescript.py
+++ examples/custom_numeric_type/examplescript.py
@@ -1,14 +1,44 @@
+import sys
+
+sys.path.append('./')
+
 import eigenpy_example_custom_numeric_type as example
 
-x = example.MpfrComplex(2)  # the number 2, in variable precision as a complex number
-
-import numpy as np
-
-M = np.zeros((3,4),dtype=example.MpfrComplex)  # make an array of the custom numeric type
-
-print(M)
 
 
-example.set_to_ones(M)
+def try_things(num_type):
 
-print(M)
+	print(f'testing {num_type}')
+
+	x = num_type(2)  # the number 2, in variable precision as a complex number
+
+	import numpy as np
+
+	print('making array from empty WITH conversion')
+	A = np.array( np.empty( (3,4)).astype(np.int64),dtype=num_type)
+
+	print(A)
+
+	print('making array from zeros WITH conversion')
+	M = np.array( np.zeros( (3,4)).astype(np.int64),dtype=num_type)  # make an array of the custom numeric type
+
+	print(M)
+
+	assert(M[0,0] == num_type(0))
+
+
+	example.set_to_ones(M)
+
+
+	assert(M[0,0] == num_type(1))
+
+
+	print(M)
+
+	print('making zeros without conversion')
+	B = np.zeros( (4,5), dtype=num_type)
+	print(B)
+
+
+try_things(example.MpfrFloat)
+try_things(example.MpfrComplex)
