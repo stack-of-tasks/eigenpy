81767358355086b252d7d21be0c0a659eaaee238
Justin Carpentier <justin.carpentier@inria.fr>
2025-01-09 23:46:27 +0100

    test/custom: use more classic allocator

--- unittest/python/test_user_type.py
+++ unittest/python/test_user_type.py
@@ -9,7 +9,7 @@ cols = 20
 
 def test(dtype):
     rng = np.random.default_rng()
-    mat = np.array(np.ones((rows, cols)), dtype=dtype)
+    mat = np.ones((rows, cols), dtype=dtype)
     mat = rng.random((rows, cols)).astype(dtype)
     mat_copy = mat.copy()
     assert (mat == mat_copy).all()
